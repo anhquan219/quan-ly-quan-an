6bb587683c707c2dd15341824f644c058fbe6810
Nguyen Anh Quan <quankull73@gmail.com>
2024-06-17 19:13:47 +0700

    Edit Account

--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -28,10 +28,17 @@ export const useGetAccountList = () => {
   });
 };
 
-export const useGetAccount = ({ id }: { id: number }) => {
+export const useGetAccount = ({
+  id,
+  enabled,
+}: {
+  id: number;
+  enabled: boolean;
+}) => {
   return useQuery({
-    queryKey: ["account", id],
+    queryKey: ["accounts", id],
     queryFn: () => accountApiRequest.getEmployee(id),
+    enabled: enabled, // Có gọi API hay không
   });
 };
 
@@ -62,6 +69,7 @@ export const useUpdateAccountMutation = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["accounts"],
+        exact: true, // Chỉ các Query có key chính xác là ["accounts"] mới Query lại data
       });
     },
   });
