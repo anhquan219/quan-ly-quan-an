a88054f1955476110bd4f3243753571efb72eb54
Nguyen Anh Quan <quankull73@gmail.com>
2024-06-22 08:25:40 +0700

    Custom Canvas QRcode

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -16,12 +16,37 @@ export default function QRcodeTable({
   const linkUrl = getTableLink({ token, tableNumber });
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    QRCode.toCanvas(canvas, linkUrl, function (error) {
+    const canvas = canvasRef.current!;
+    canvas.height = width + 70;
+    canvas.width = width;
+    const canvasContext = canvas.getContext("2d")!;
+    // Fill mầu trắng
+    canvasContext.fillStyle = "#fff";
+    // Điền thông tin vào thẻ canvas từ pixel 0,0 đến canvas.width, canvas.height
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.font = "20px Arial";
+    canvasContext.fillStyle = "#000";
+    canvasContext.textAlign = "center";
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    );
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    );
+
+    // Taoj thẻ canvas
+    const virtalCanvas = document.createElement("canvas");
+
+    QRCode.toCanvas(virtalCanvas, linkUrl, function (error) {
       if (error) console.error(error);
-      console.log("success!");
+      // Vẽ QR virtalCanvas vào canvasContext
+      canvasContext.drawImage(virtalCanvas, 0, 0, width, width);
     });
-  }, [linkUrl]);
+  }, [linkUrl, width]);
 
   return <canvas ref={canvasRef} />;
 }
